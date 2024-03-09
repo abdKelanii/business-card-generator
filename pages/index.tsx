a681b86708e6e8cfdac132ec9dec179f7c54fd02
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-03-10 00:22:51 +0300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,3 @@
-// Maltepe University
-// Graduation Project - business Card Generator
-// Abd Alsalam Hijazi Kelani - 19 07 04 815
-// Wael Rashed - 19 07 04 822
-
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
