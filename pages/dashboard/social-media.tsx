f8ee523b1f8c3cae4a0eb53a83f0e988ef534571
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-05-11 04:02:43 +0300

    Social Media Updating And Getting 100%

--- pages/dashboard/social-media.tsx
+++ pages/dashboard/social-media.tsx
@@ -8,7 +8,6 @@ import { Button } from "@chakra-ui/react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getDatabase, ref, get, set } from "firebase/database";
 import { toast } from "sonner";
-import { validateEmail } from "@/helpers/validators";
 
 const SocialMedia = () => {
   const [links, setLinks] = useState({
@@ -44,7 +43,7 @@ const SocialMedia = () => {
   const fetchData = async (userId: string) => {
     try {
       const db = getDatabase();
-      const snapshot = await get(ref(db, "users/links" + userId));
+      const snapshot = await get(ref(db, `users/${userId}/links`));
       if (snapshot.exists()) {
         const data = snapshot.val();
         setLinks(data);
@@ -54,6 +53,63 @@ const SocialMedia = () => {
     }
   };
 
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setLinks({
+      ...links,
+      [name]: value,
+    });
+  };
+
+  const validateSocialMediaLinks = () => {
+    let errorMessage = "";
+
+    if (
+      links.linkedIn &&
+      !links.linkedIn.startsWith("https://www.linkedin.com/")
+    ) {
+      errorMessage +=
+        "LinkedIn link should start with 'https://www.linkedin.com/'.\n";
+    }
+
+    if (
+      links.instagram &&
+      !links.instagram.startsWith("https://www.instagram.com/")
+    ) {
+      errorMessage +=
+        "Instagram link should start with 'https://www.instagram.com/'.\n";
+    }
+
+    if (
+      links.facebook &&
+      !links.facebook.startsWith("https://www.facebook.com/")
+    ) {
+      errorMessage +=
+        "Facebook link should start with 'https://www.facebook.com/'.\n";
+    }
+
+    return errorMessage;
+  };
+
+  const handleSave = async () => {
+    let errorMessage = validateSocialMediaLinks();
+
+    if (errorMessage) {
+      toast.error(errorMessage);
+    } else {
+      try {
+        const db = getDatabase();
+        await set(ref(db, `users/${userId}/links`), {
+          ...links,
+        });
+        toast.success("Social media links saved successfully!");
+        console.log("Data saved successfully!");
+      } catch (error: any) {
+        console.error("Error saving data:", error);
+        toast.error(error.message);
+      }
+    }
+  };
   return (
     <>
       <Head>
@@ -69,25 +125,51 @@ const SocialMedia = () => {
               <div className="grid grid-cols-2 gap-4">
                 <div className="space-y-2">
                   <Label htmlFor="linkedin">LinkedIn</Label>
-                  <Input defaultValue="linkedin.com/in/johndoe" id="linkedin" />
+                  <Input
+                    defaultValue="linkedin.com/in/johndoe"
+                    id="linkedIn"
+                    name="linkedIn"
+                    value={links.linkedIn}
+                    onChange={handleChange}
+                  />
                 </div>
                 <div className="space-y-2">
                   <Label htmlFor="X">X</Label>
-                  <Input defaultValue="@johndoe" id="x" />
+                  <Input
+                    defaultValue="@johndoe"
+                    id="x"
+                    name="x"
+                    value={links.x}
+                    onChange={handleChange}
+                  />
                 </div>
               </div>
               <div className="grid grid-cols-2 gap-4">
                 <div className="space-y-2">
                   <Label htmlFor="instagram">Instagram</Label>
-                  <Input defaultValue="instagram.com/johndoe" id="instagram" />
+                  <Input
+                    defaultValue="instagram.com/johndoe"
+                    id="instagram"
+                    name="instagram"
+                    value={links.instagram}
+                    onChange={handleChange}
+                  />
                 </div>
                 <div className="space-y-2">
                   <Label htmlFor="facebook">Facebook</Label>
-                  <Input defaultValue="facebook.com/johndoe" id="facebook" />
+                  <Input
+                    defaultValue="facebook.com/johndoe"
+                    id="facebook"
+                    name="facebook"
+                    value={links.facebook}
+                    onChange={handleChange}
+                  />
                 </div>
               </div>
               <div className="flex justify-end">
-                <Button className="mt-5 w-24">Save</Button>
+                <Button onClick={handleSave} className="mt-5 w-24">
+                  Save
+                </Button>
               </div>
             </CardContent>
           </Card>
