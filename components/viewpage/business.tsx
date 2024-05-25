11939da737af6ea77e8781991bb59ceb32c4f605
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2024-05-25 22:50:48 +0300

    Improve View Page

--- components/viewpage/business.tsx
+++ components/viewpage/business.tsx
@@ -1,6 +1,10 @@
 import { User } from "@/utils/fetchUser";
 import React from "react";
-import { IconBuildingEstate, IconBriefcase, IconWorld } from "@tabler/icons-react";
+import {
+  IconBuildingEstate,
+  IconBriefcase,
+  IconWorld,
+} from "@tabler/icons-react";
 
 interface ViewProps {
   user: User;
@@ -11,24 +15,30 @@ const Personal = ({ user }: ViewProps) => {
     <div className="space-y-4">
       <h3 className="text-lg font-semibold">Professional Details</h3>
       <div className="space-y-2">
-        <div className="flex items-center space-x-2">
-          <IconBuildingEstate className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-          <span className="text-gray-700 dark:text-gray-300">
-            {user.business.company}
-          </span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <IconBriefcase className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-          <span className="text-gray-700 dark:text-gray-300">
-            {user.business.jobTitle}
-          </span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <IconWorld className="h-5 w-5 text-gray-500 dark:text-gray-400" />
-          <span className="text-gray-700 dark:text-gray-300">
-            {user.business.website}
-          </span>
-        </div>
+        {user.business.company && (
+          <div className="flex items-center space-x-2">
+            <IconBuildingEstate className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+            <span className="text-gray-700 dark:text-gray-300">
+              {user.business.company}
+            </span>
+          </div>
+        )}
+        {user.business.jobTitle && (
+          <div className="flex items-center space-x-2">
+            <IconBriefcase className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+            <span className="text-gray-700 dark:text-gray-300">
+              {user.business.jobTitle}
+            </span>
+          </div>
+        )}
+        {user.business.website && (
+          <div className="flex items-center space-x-2">
+            <IconWorld className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+            <span className="text-gray-700 dark:text-gray-300">
+              {user.business.website}
+            </span>
+          </div>
+        )}
       </div>
       <div className="text-gray-700 dark:text-gray-300">
         {/* {user.business.bio} */}
